a5ee7ffe9d30fe62ff8768796ec1e44029c815bd
manriquezRaul <r.manriquezr03@ufromail.cl>
2026-06-20 20:20:53 -0400

    feat: crear estructura de archivos base para las pantallas de Figma

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-bold">Historial de Notas (Plano Figma)</h2>
+      <p>Espacio reservado para el buscador y las tarjetas de MongoDB.</p>
+    </div>
+  );
+}
